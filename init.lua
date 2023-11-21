b029623782fe36d39a6f1734ffdca0d130d053c2
Kresna Laksana <baguskresnalaksana@gmail.com>
2023-11-21 14:40:33 +0800

    FEATURE : add vim options
    add several option for indenting, numbering and others

---
+++ init.lua
@@ -0,0 +1,46 @@
+--- variables ---
+local opt = vim.opt
+
+--- options ---
+opt.laststatus = 3 -- global statusline
+opt.showmode = false
+
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+
+-- Indenting
+opt.expandtab = false
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+
+opt.fillchars = { eob = " " }
+opt.ignorecase = true
+opt.smartcase = true
+opt.mouse = "a"
+
+-- Numbers
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 4
+opt.ruler = true
+
+-- disable nvim intro
+opt.shortmess:append "sI"
+
+opt.signcolumn = "yes"
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.timeoutlen = 400
+opt.undofile = true
+opt.wrap = false
+opt.linebreak = false
+
+-- interval for writing swap file to disk, also used by gitsigns
+opt.updatetime = 250
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append "<>[]hl"
